a710cb92449e96217e26985fee0bcba0fdace467
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-07-04 16:14:20 -0700

    try number one

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,4 +5,19 @@ quetsion
 """
 
 def canUnlockAll(boxes):
+    """ description of the function """
+    if not boxes:
+        return False
+    visited = [False] * len(boxes)
+    visited[0] = True
+
+    stack = [0]
+
+    while stack:
+        box = stack.pop()
+        for key in boxes[box]:
+            if 0 <= key < len(boxes) and not visited[key]:
+                visited[key] = True
+                stack.append(key)
+    return all(visited)
     pass
