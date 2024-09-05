17df98d97af43e350069d256b8059153ed0f4652
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-09-06 01:52:59 +0200

    task 0

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-""" 
-this python file is for solving primegame problem
 """
+Define isWineer function, a solution to the Prime Game problem
+"""
+
 
 def primes(n):
     """Return list of prime numbers between 1 and n inclusive
